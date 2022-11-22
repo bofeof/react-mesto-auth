28586d0f6d334ba13513c292027b4d31520bf4b1
Polina Che <perhaybe@gmail.com>
2022-11-22 22:13:51 +0300

    fix: classic and burger menu displaying

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import { LoggedInContext } from "../contexts/CurrentUserContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
 
 export default function Header({ onLogOut }) {
+  const [menuOpened, setMenuOpened] = useState(false);
   const userLogIn = useContext(LoggedInContext);
   const location = useLocation();
   const currentUrl = location.pathname;
   const history = useHistory();
+
+  // update window-width for displaying menu: burger or classic
+  const [windowWidth, setWindowWidth] = useState();
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  });
+
+  function updateSize() {
+    setWindowWidth(window.innerWidth);
+  }
 
   function handleLogOut() {
     onLogOut();
@@ -21,32 +34,92 @@ export default function Header({ onLogOut }) {
     }
   }
 
-  return (
+  function handleOpenMenu() {
+    setMenuOpened(!menuOpened);
+  }
+
+  return userLogIn ? (
+    // LOGIN
     <header className="header">
       <div className="header__logo"></div>
-      {userLogIn ? (
-        <div className="header__user">
-          <p className="header__email">email@email</p>
-          <button
-            className="header__button header__logout-button"
-            onClick={handleLogOut}>
-            Выйти
-          </button>
-        </div>
+      {windowWidth >= 768 ? (
+        // classic menu
+        <>
+          <div
+            className="header__menu"
+            onClick={handleOpenMenu}
+            style={{ display: "none" }}>
+            <div className="header__burger-close"></div>
+            <div className="header__menu-burger">
+              <span className="header__menu-line"></span>
+              <span className="header__menu-line"></span>
+              <span className="header__menu-line"></span>
+            </div>
+          </div>
+
+          <div className="header__user" style={{ display: "flex" }}>
+            <p className="header__email">email@email</p>
+            <button
+              style={{ margin: "0 0 40px" }}
+              className="header__button header__logout-button"
+              onClick={handleLogOut}>
+              Выйти
+            </button>
+          </div>
+        </>
       ) : (
-        <div className="header__user">
-          {currentUrl === "/sign-in" && (
-            <button className="header__button" onClick={handleRedirect}>
-              Регистрация
+        //  burger-menu
+        <>
+          <div className="header__menu" onClick={handleOpenMenu}>
+            {menuOpened ? (
+              <div className="header__burger-close"></div>
+            ) : (
+              <div className="header__menu-burger">
+                <span className="header__menu-line"></span>
+                <span className="header__menu-line"></span>
+                <span className="header__menu-line"></span>
+              </div>
+            )}
+          </div>
+
+          <div
+            className="header__user"
+            style={{ display: menuOpened ? "flex" : "none" }}>
+            <p className="header__email">email@email</p>
+            <button
+              style={{ margin: "0 0 40px" }}
+              className="header__button header__logout-button"
+              onClick={handleLogOut}>
+              Выйти
             </button>
-          )}
-          {currentUrl === "/sign-up" && (
-            <button className="header__button" onClick={handleRedirect}>
-              Войти
-            </button>
-          )}
-        </div>
+          </div>
+        </>
       )}
+    </header>
+  ) : (
+    // LOGOUT
+    <header
+      className="header"
+      style={{ gridTemplateAreas: `'header__logo header__menu header__user'` }}>
+      <div className="header__logo"></div>
+      <div className="header__user">
+        <div className="header__burger-close" style={{ display: "none" }}></div>
+        <div className="header__menu-burger" style={{ display: "none" }}>
+          <span className="header__menu-line"></span>
+          <span className="header__menu-line"></span>
+          <span className="header__menu-line"></span>
+        </div>
+        {currentUrl === "/sign-in" && (
+          <button className="header__button" onClick={handleRedirect}>
+            Регистрация
+          </button>
+        )}
+        {currentUrl === "/sign-up" && (
+          <button className="header__button" onClick={handleRedirect}>
+            Войти
+          </button>
+        )}
+      </div>
     </header>
   );
 }
