01d5c70b35130ecb63e1f323a8b60ef3d73b6b36
Polina Che <perhaybe@gmail.com>
2022-11-24 12:25:10 +0300

    fix: browserrouter is replaced by hashrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
